37f63d45438bc57b9b3a1d789f3c816c64a72338
Phillip Webb <pwebb@pivotal.io>
2018-03-31 09:02:32 -0700

    Hacking

--- springboot/src/main/java/org/objectstyle/springboot/Main.java
+++ springboot/src/main/java/org/objectstyle/springboot/Main.java
@@ -1,12 +1,29 @@
 package org.objectstyle.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.objectstyle.di.service.Service;
+import org.objectstyle.di.service.ServiceImpl;
+import org.objectstyle.di.service.SubService;
+import org.objectstyle.di.service.SubServiceImpl;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@Component
 public class Main {
 
+   @Bean
+    public Service service(SubService subService) {
+        return new ServiceImpl(subService);
+    }
+
+    @Bean
+    public SubService subService() {
+        return new SubServiceImpl();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+    		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+    		System.out.println(context.getBean(Service.class).doIt());
     }
 }
